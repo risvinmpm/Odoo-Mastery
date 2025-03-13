b1d86a298b6ba0fc9f16b909e31447d6426ac4f1
risvinmpm <risvinmp39@gmail.com>
2025-03-13 12:21:14 +0530

    first commit

---
+++ html.creativegigstf.com/charles/js/map-script.js
@@ -0,0 +1,194 @@
+"use strict";
+$(function() {
+    var mapStyle = 
+    				[ 
+				    {
+				        "featureType": "water",
+				        "elementType": "geometry",
+				        "stylers": [
+				            {
+				                "color": "#e9e9e9"
+				            },
+				            {
+				                "lightness": 17
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "landscape",
+				        "elementType": "geometry",
+				        "stylers": [
+				            {
+				                "color": "#f5f5f5"
+				            },
+				            {
+				                "lightness": 20
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "road.highway",
+				        "elementType": "geometry.fill",
+				        "stylers": [
+				            {
+				                "color": "#ffffff"
+				            },
+				            {
+				                "lightness": 17
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "road.highway",
+				        "elementType": "geometry.stroke",
+				        "stylers": [
+				            {
+				                "color": "#ffffff"
+				            },
+				            {
+				                "lightness": 29
+				            },
+				            {
+				                "weight": 0.2
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "road.arterial",
+				        "elementType": "geometry",
+				        "stylers": [
+				            {
+				                "color": "#ffffff"
+				            },
+				            {
+				                "lightness": 18
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "road.local",
+				        "elementType": "geometry",
+				        "stylers": [
+				            {
+				                "color": "#ffffff"
+				            },
+				            {
+				                "lightness": 16
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "poi",
+				        "elementType": "geometry",
+				        "stylers": [
+				            {
+				                "color": "#f5f5f5"
+				            },
+				            {
+				                "lightness": 21
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "poi.park",
+				        "elementType": "geometry",
+				        "stylers": [
+				            {
+				                "color": "#dedede"
+				            },
+				            {
+				                "lightness": 21
+				            }
+				        ]
+				    },
+				    {
+				        "elementType": "labels.text.stroke",
+				        "stylers": [
+				            {
+				                "visibility": "on"
+				            },
+				            {
+				                "color": "#ffffff"
+				            },
+				            {
+				                "lightness": 16
+				            }
+				        ]
+				    },
+				    {
+				        "elementType": "labels.text.fill",
+				        "stylers": [
+				            {
+				                "saturation": 36
+				            },
+				            {
+				                "color": "#333333"
+				            },
+				            {
+				                "lightness": 40
+				            }
+				        ]
+				    },
+				    {
+				        "elementType": "labels.icon",
+				        "stylers": [
+				            {
+				                "visibility": "off"
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "transit",
+				        "elementType": "geometry",
+				        "stylers": [
+				            {
+				                "color": "#f2f2f2"
+				            },
+				            {
+				                "lightness": 19
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "administrative",
+				        "elementType": "geometry.fill",
+				        "stylers": [
+				            {
+				                "color": "#fefefe"
+				            },
+				            {
+				                "lightness": 20
+				            }
+				        ]
+				    },
+				    {
+				        "featureType": "administrative",
+				        "elementType": "geometry.stroke",
+				        "stylers": [
+				            {
+				                "color": "#fefefe"
+				            },
+				            {
+				                "lightness": 17
+				            },
+				            {
+				                "weight": 1.2
+				            }
+				        ]
+				    }
+				];
+
+    // Create the map centered on Cyberpark, Calicut (Kozhikode)
+    var map = new google.maps.Map($('.map-canvas')[0], {
+        zoom: 15,  // You may want a slightly higher zoom to focus more on the area
+        styles: mapStyle,
+        scrollwheel: false,
+        center: new google.maps.LatLng(11.2564, 75.7802)  // Updated to Cyberpark's lat/lng
+    });
+
+    // Add a marker at Cyberpark, Calicut
+    var marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(11.2564, 75.7802)  // Updated marker position
+    });
+});
